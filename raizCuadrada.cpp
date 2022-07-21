1665d622014a2cf6e10ef125da37e8c5febda72d
HamlettGamez <106714595+HamlettGamez@users.noreply.github.com>
2022-07-20 21:30:30 -0600

    Add files via upload

---
+++ raizCuadrada.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+main(){
+    double numero,raiz;
+    cout<<"ingrese un numero:";
+	cin>>numero;
+    raiz=sqrt(numero);
+    cout<<"la raiz cuadra de "<<numero<<" es:"<<raiz;
+ return 0;
+}

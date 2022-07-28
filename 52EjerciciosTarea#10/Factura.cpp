768d84f5f80a0b0be8a2b3b8f1f22a6dd076f267
HamlettGamez <106714595+HamlettGamez@users.noreply.github.com>
2022-07-27 20:09:17 -0600

    Add files via upload

---
+++ 52EjerciciosTarea#10/Factura.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int srgc,char const *argv[])
+{
+	double subtotal=0;
+	double total=0;
+	double Impuesto=0.15;
+	
+
+	cout<< "Ingrese el subtotal de la  factura: ";
+	cin >> subtotal;
+	
+	total= subtotal + (subtotal *0.15) ; 
+	
+	cout << "Total a cancelar es: " <<total;
+	
+	
+	return 0;
+}
